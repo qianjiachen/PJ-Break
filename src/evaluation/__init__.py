cb810ac6c30013728e7ce6c73b76b1d533f82572
qianjiachen <2298380896@qq.com>
2026-01-03 04:04:26 +0800

    Remove HumanEvaluator reference

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -3,9 +3,8 @@
 from .judges import Judge, ClaudeJudge, LlamaGuardJudge, KeywordJudge
 from .multi_judge import MultiJudgeSystem
 from .metrics import MetricsCalculator
-from .human_eval import HumanEvaluator
 
 __all__ = [
     "Judge", "ClaudeJudge", "LlamaGuardJudge", "KeywordJudge",
-    "MultiJudgeSystem", "MetricsCalculator", "HumanEvaluator"
+    "MultiJudgeSystem", "MetricsCalculator"
 ]
